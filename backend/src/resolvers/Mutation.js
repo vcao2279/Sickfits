2847adf478aace8908cf5e2b235ca8c33afbd59e
Vu Cao <cdhvu@yahoo.com>
2018-11-04 22:30:42 -0800

    Add resolvers in Mutation.js and Query.js

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if they're logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-module.exports = mutations;
+    console.log(item);
+    return item;
+  }
+};
+
+module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+// if we want to use prisma db query directly.
+// const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  //   items: forwardTo("db") ==> if we want to use prisma db query directly.
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items();
+    return items;
+  }
+};
 
 module.exports = Query;
